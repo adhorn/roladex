40bf149d8680e48c8154994fe6a2cb23bc5737d8
Adrian Hornsby <adhorn@amazon.com>
2017-08-22 14:01:57 +0300

    fixed indentation v2

--- iot_robot.py
+++ iot_robot.py
@@ -1,15 +1,8 @@
 import time
-
-from gopigo import *
-
 import iot_connect
+from gopigo import *
 from iot_topics import *
 
-
-def scan():
-    dist = (str)(us_dist(15))
-    message = "The object is "+dist+" centimeters away"
-    return message
 
 def callbackMove(client, userdata, message):
     print "Topic="+message.topic
@@ -31,47 +24,19 @@ def callbackMove(client, userdata, message):
         decrease_speed()    # Decrease speed
     else:
         print "Wrong Command, Please Enter Again"
-        time.sleep(1)
-        stop()
-
-
-def callbackScan(client, userdata, message):
-    print "Topic="+message.topic
-    print "Message="+message.payload
-    cmd = message.payload
-    if cmd == "scan":
-        message = scan()
-    elif cmd == "left":
-        global angle
-        angle = angle + 30
-        if (angle > 180):
-            angle = 180
-            servo(angle)
-    elif cmd == "right":
-        global angle
-        angle = angle - 30
-        if (angle < 0):
-            angle = 0
-            servo(angle)
-    elif cmd == "reset":
-        servo(90)
-    else:
-        print "Wrong Command, Please Enter Again"
+    time.sleep(1)
+    stop()
 
 
 # Reset servo to center position
 enable_servo()
-angle = 90
-servo(angle)
+servo(90)
 
 # Connect to IoT Gateway and subscribe to topics
 client = iot_connect.connectIot()
 client.subscribe(topicMove, 1, callbackMove)
-client.subscribe(topicScan, 1, callbackScan)
 
 while True:
     time.sleep(10)
     client.unsubscribe(topicMove)
-    client.unsubscribe(topicScan)
     iot_connect.disconnectIot(client)
-    disable_servo()
